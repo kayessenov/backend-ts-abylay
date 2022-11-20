09aec04e9c6d19d14fa6ae4e76ba50704d08de88
kayessenov <kadievv06@gmail.com>
2022-11-20 22:25:00 +0600

    second com

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-import http from 'http'
+import express from 'express'
+
+const app = express();
+const port = 3005;
+
+app.listen(port, (req,res) => {
+    console.log(`Server started http://localhost:${port}`)
+})
 
