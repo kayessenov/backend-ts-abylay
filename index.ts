19178169d65512d8cfdb621a2271d261d68572f5
kayessenov <kadievv06@gmail.com>
2022-11-20 23:05:53 +0600

    three comm

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-import express from 'express'
+import express, { Request, Response, NextFunction} from 'express'
 
 const app = express();
 const port = 3005;
 
-app.listen(port, (req,res) => {
+app.listen(port, () => {
     console.log(`Server started http://localhost:${port}`)
 })
 
